bcce05e6a0ca524dc06c75def0302031ba6ff356
Romina Cannizzaro <rominacannizzaro@gmail.com>
2021-10-11 19:54:53 +0300

    Add app.js and select song, play button, outline and video elements

---
+++ app.js
@@ -0,0 +1,8 @@
+const app = () => {   
+    const song = document.querySelector('.song');  
+    const play = document.querySelector('.play'); 
+    const outline = document.querySelector('.moving-outline circle');  
+    const video = document.querySelector('.vid-container video'); 
+    }
+
+    app();
